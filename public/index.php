6d1f1c257528e7d80ae8a771c756026704c93575
noobmaster122 <brahimi.a.farouk@gmail.com>
2024-08-04 12:27:11 +0200

    added const for project base url

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
+<?php
+function getBaseUrl() {
+    // Check if the request is secure (HTTPS) or not
+    $isSecure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
+
+    // Get the HTTP or HTTPS protocol
+    $protocol = $isSecure ? 'https://' : 'http://';
+    //$protocol = 'https://';
+    // Get the server name (host)
+    $host = $_SERVER['HTTP_HOST'];
+
+    // Get the base path (if your project is not in the root directory)
+    $basePath = dirname($_SERVER['PHP_SELF']);
+
+    // Explode the base path and keep only the first segment
+    $basePathSegments = explode('/', trim($basePath, '/'));
+    $firstFolder = isset($basePathSegments[0]) ? $basePathSegments[0] : '';
+
+    $firstFolder = empty($firstFolder) ? '' : '/' . $firstFolder;
+    // Concatenate and return the base URL
+    return $protocol . $host . $firstFolder;
+}
+define("GPI_PROJECT_ROOT_FOLDER", getBaseUrl());
+?>
 <!DOCTYPE html>
 <html lang="en">
 
