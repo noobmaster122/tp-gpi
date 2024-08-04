6e1514f2421a2c0b4e8bb2f2b82482a243198be3
noobmaster122 <brahimi.a.farouk@gmail.com>
2024-08-04 12:27:58 +0200

    updated header to match tamplate

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,32 +1,40 @@
-<header class="bg-white shadow-sm">
+<header class="bg-white shadow-sm pb-8">
     <div class="container mx-auto py-2 px-6 flex justify-between items-center">
         <div class="flex items-center space-x-4">
             <span class="text-gray-600 text-xs">Shop by Phone <span class="font-bold">(01) 123 456 SM</span></span>
             <a href="#" class="text-blue-600 text-xs font-semibold">Live Chat With Us</a>
         </div>
-        <div class="flex items-center space-x-6">
-            <a href="#" class="text-gray-800 font-medium text-xs">My Account</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">My Cart (2)</a>
+        <div class="flex items-center space-x-2">
+            <a href="#" class="text-blue-600 font-medium text-xs">My Account</a>
+            <svg class="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 14l-6-6h12l-6 6z"></path>
+            </svg>
+            <span class="">|</span>
+            <a href="#" class="text-blue-600 font-medium text-xs">My Cart (2)</a>
+            <svg class="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 14l-6-6h12l-6 6z"></path>
+            </svg>
         </div>
     </div>
     <div class="container mx-auto py-4 px-6 flex justify-between items-center">
-        <div class="text-3xl font-bold text-blue-600">softmarket</div>
-        <nav class="flex space-x-8">
-            <a href="#" class="text-gray-800 font-medium text-xs">OFFICE</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">MULTIMEDIA</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">DESIGN</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">DEVELOPER</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">UTILITIES</a>
-            <a href="#" class="text-gray-800 font-medium text-xs">GAMES</a>
-        </nav>
-        <div class="relative">
-            <input type="text"  class="border border-gray-300 px-4 py-2  pr-10 w-full">
-            <button class="absolute right-0 top-0 mt-3 mr-3 text-gray-600">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M12.9 14.32a8 8 0 111.41-1.41l4.36 4.36a1 1 0 01-1.42 1.42l-4.35-4.36zM8 14a6 6 0 100-12 6 6 0 000 12z" clip-rule="evenodd" />
-                </svg>
-            </button>
+        <div class="text-3xl font-bold text-blue-600"><img src="<?= GPI_PROJECT_ROOT_FOLDER . '/public/assets/img-01.png'; ?>" alt="website logo"></div>
+        <div class="flex justify-between itexms-center gap-10">
+            <nav class="flex space-x-8 items-center">
+                <a href="#" class="text-gray-800 font-medium text-xs">OFFICE</a>
+                <a href="#" class="text-gray-800 font-medium text-xs">MULTIMEDIA</a>
+                <a href="#" class="text-gray-800 font-medium text-xs">DESIGN</a>
+                <a href="#" class="text-gray-800 font-medium text-xs">DEVELOPER</a>
+                <a href="#" class="text-gray-800 font-medium text-xs">UTILITIES</a>
+                <a href="#" class="text-gray-800 font-medium text-xs">GAMES</a>
+            </nav>
+            <div class="relative">
+                <input type="text" class="border border-gray-300 px-4 py-2  pr-10 w-full">
+                <button class="absolute right-0 top-0 mt-3 mr-3 text-gray-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M12.9 14.32a8 8 0 111.41-1.41l4.36 4.36a1 1 0 01-1.42 1.42l-4.35-4.36zM8 14a6 6 0 100-12 6 6 0 000 12z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </div>
         </div>
-
     </div>
 </header>
